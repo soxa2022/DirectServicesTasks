a9282c2d3b3923fdf85761ace295452079a33d98
Anton Ivanov <105536897+soxa2022@users.noreply.github.com>
2023-05-09 09:14:27 +0300

    Delete TaskDjango directory

--- TaskDjango/README.md
+++
@@ -1,3 +0,0 @@
-- WEB app for weather forcast using Django for back-end and Bootstrap CSS , JQuery JS for front-end.
-- Postgresql DB for saving information.
-
--- TaskDjango/weather/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("get_weather", views.get_weather, name="get_weather"),
-]
